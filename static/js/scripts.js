3ad704362782e42ea556b2f2eb4731f5878f82bb
bikerlfh <bikerlfh@hotmail.com>
2017-04-26 16:59:27 -0500

    pedido

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -405,3 +405,33 @@ $(document).ready(function(){
 $.ajaxPrefilter(function( options, original_Options, jqXHR ) {
     options.async = true;
 });
+(function(a) {
+    a.fn.validarCampo = function(options) {
+    switch(options.type){
+      case 'number':
+        characters = '0123456789'
+        break;
+      case 'char':
+        characters = 'abcdefghijklmnñopqrstuvwxyzáéíóú '
+        break;
+      default:
+        characters = "0123456789abcdefghijklmnñopqrstuvwxyzáéíóúABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚ "
+        break;
+    }
+        max_length = options.length || null;
+        a(this).on({
+            keypress: function(a) {
+              if(max_length == null || $(this).val().length <= max_length-1){
+                
+                  var c = a.which,
+                      d = a.keyCode,
+                      e = String.fromCharCode(c).toLowerCase(),
+                      f = characters;
+                  (-1 != f.indexOf(e) || 9 == d || 37 != c && 37 == d || 39 == d && 39 != c || 8 == d || 46 == d && 46 != c) && 161 != c || a.preventDefault()
+              }
+              else
+                a.preventDefault();
+            }
+        })
+    }
+})(jQuery);
