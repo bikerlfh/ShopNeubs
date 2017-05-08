465a2f6e912b56823aba254fcf771688388e4edb
bikerlfh <bikerlfh@hotmail.com>
2017-05-08 15:58:40 -0500

    img

--- ShopNeubs/hosts.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django_hosts import patterns, host
-from django.contrib import admin
-
-host_patterns = patterns('',
-    host(r'www.shopneubs.com.co:8000', settings.ROOT_URLCONF, name='shopneubs'),
-    #host(r'admin', 'ShopNeubs.hostsconf.urls', name='wildcard'),
-    host(r'board', 'ShopNeubs.hostsconf.urls', name='wildcard'),
-)
