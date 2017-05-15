902fa60681cb0069eaf18c27eaa43ea75984bce3
bikerlfh <bikerlfh@hotmail.com>
2017-05-15 16:29:50 -0500

    forms

--- inventario/forms.py
+++ inventario/forms.py
@@ -5,14 +5,29 @@ from .models import SaldoInventario,Promocion
 class SaldoInventarioForm(forms.ModelForm):
 	class Meta:
 		model = SaldoInventario
-		fields = ['producto','proveedor','cantidad','costoTotal','precioVentaUnitario']
+		fields = ['producto','proveedor','cantidad','costoTotal','precioOferta','precioCompraUnitario','precioVentaUnitario']
 	# Se valida que si el precioVentaUnitario es modificado por el usuario, 
 	# no se permita que éste sea menor al costo por unidad
+	
+
 	def clean_precioVentaUnitario(self):
 		data = self.cleaned_data
 		if data['cantidad'] > 0 and data['precioVentaUnitario'] > 0 and  data['precioVentaUnitario'] <= (data['costoTotal']/data['cantidad']):
 			raise ValidationError("El precio de Venta no debe ser menor al costo por unidad")
 		return data['precioVentaUnitario']
+
+	def clean_precioCompraUnitario(self):
+		data = self.cleaned_data
+		if data['cantidad'] > 0 and data['costoTotal'] > 0 and data['precioCompraUnitario'] <= 0:
+			raise ValidationError("Favor ingresar el Precio Compra Unitario")
+		return data['precioCompraUnitario']
+
+	def clean_precioOferta(self):
+		data = self.cleaned_data
+		if data['cantidad'] > 0 and data['precioOferta'] > 0 and data['precioVentaUnitario'] >= data['precioOferta']:
+			raise ValidationError("El precio Oferta debe ser mayor al precio Venta Unitario")
+		return data['precioOferta']
+
 # class PromocionForm(forms.ModelForm):
 #     class Meta:
 #         model = Promocion
